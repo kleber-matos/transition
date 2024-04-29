f640dbe1d83f0ae3e0dd390f704d094b9c95d572
kleber-matos <klebermatos2019@icloud.com>
2024-04-29 08:33:57 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+/** @format */
+
+import React, { useState } from "react";
+import styled from "styled-components";
+import * as S from "./styled";
+
+const Carregando = styled.div`
+  background-color: #60ee35;
+  width: 100px;
+  height: 20px;
+  transition: 10s;
+
+  ${({ ativo }) =>
+    ativo &&
+    `
+    width: 100%;
+  `}
+`;
+
+export default function App() {
+  const [ativo, setAtivo] = useState(false);
+  const [cor, setCor] = useState(false);
+
+  return (
+    <>
+      <Carregando ativo={ativo} onClick={() => setAtivo(!ativo)}>
+        Carregando...
+      </Carregando>
+
+      <S.Caixa cor={cor}>
+        <p>texto</p>
+      </S.Caixa>
+
+      <button onClick={() => setCor(!cor)}>mudar cor</button>
+
+      <button onClick={() => setAtivo(!ativo)}>carregando</button>
+    </>
+  );
+}
---
+++ src/styled.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import styled from "styled-components";
+
+export const Caixa = styled.div`
+  transition: 2s;
+  background-color: red;
+  width: 100px;
+  height: 100px;
+
+  ${({ cor }) =>
+    cor &&
+    `
+      background-color: yellow;
+
+  `}
+`;
