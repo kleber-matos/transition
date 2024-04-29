76a1f74bd19d65b09858f34d42367933107064bf
kleber-matos <klebermatos2019@icloud.com>
2024-04-29 13:53:46 -0300

    t1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import * as S from "./styled";
 
 export default function App() {
-  const [ativo, setAtivo] = useState(false);
-
   return (
     <>
-      <S.Titulo>trasition</S.Titulo>
-
-      <S.Carregando ativo={ativo}>carregando</S.Carregando>
-
-      <button onClick={() => setAtivo(!ativo)}>start</button>
+      <h1>trasition</h1>
     </>
   );
 }
--- src/styled.jsx
+++ src/styled.jsx
@@ -1,20 +1,3 @@
 /** @format */
 
 import styled from "styled-components";
-
-export const Titulo = styled.h1`
-  font-family: "Poppins", sans-serif;
-`;
-
-export const Carregando = styled.div`
-  background-color: greenyellow;
-  width: 40vw;
-  transition: 5s;
-
-  ${({ ativo }) =>
-    ativo &&
-    `
-        width: 100vw;
-        background-color: yellow;
-    `}
-`;
